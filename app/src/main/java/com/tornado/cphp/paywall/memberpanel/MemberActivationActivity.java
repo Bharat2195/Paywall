5875fd8797732bdedf939ad73d896ffe32cc5f10
cphp-PC\cphp <sindhavbharat2195@gmail.com>
2017-01-09 13:00:48 +0530

    Add Project changes

---
+++ app/src/main/java/com/tornado/cphp/paywall/memberpanel/MemberActivationActivity.java
@@ -0,0 +1,15 @@
+package com.tornado.cphp.paywall.memberpanel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tornado.cphp.paywall.R;
+
+public class MemberActivationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_activation);
+    }
+}
